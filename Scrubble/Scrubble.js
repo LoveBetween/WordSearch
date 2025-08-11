2417a383be3dd26d0b9f56bfa8d6423db2681db5
Miguel <39136142+LoveBetween@users.noreply.github.com>
2025-08-11 07:15:25 +0200

    Incorporate into Game object

--- Scrubble/Scrubble.js
+++ Scrubble/Scrubble.js
@@ -1,17 +1,22 @@
 class Game{
-    constructor(width, height, cellSize, p1Type, p2Type, grid, canvas){
+    constructor(width, height, cellSize, p1Type, p2Type, grid, canvas, letterBag){
+        let that = this
+        document.getElementById("scrabbleBoard").addEventListener('click', function(e) { 
+            inputHandler(document.getElementById("scrabbleBoard"), e, that)
+        });
         this.width = 15
         this.height = 15
         this.grid = grid
         if (grid == null){
             this.grid = this.initGrid(15, 15) //15,15 for now
         }
-        this.turn = 1
-        this.p1 = {type:p1Type, letters:"", score:0}
-        this.p2 = {type:p2Type, letters:"", score:0}
+        this.turn = 0
+        this.p1 = {name:"p1", type:p1Type, letters:"", score:0}
+        this.p2 = {name: "p2", type:p2Type, letters:"", score:0}
         this.cellSize = cellSize
         this.highlightedCell = null
         this.canvas = canvas
+        this.letterBag = letterBag
     }
     initGrid(width, height){
         return Array(width).fill(null).map(() => Array(height).fill("_"))
@@ -31,13 +36,109 @@ class Game{
     drawGrid(){
         displayGrid(this.grid, standardMultiGrid, standardColor, this.canvas, this.cellSize, this.highlightedCell, valeurObj, true)
     }
+    updateScore(player, score){
+        player.score += score
+        // displayScore
+    }
+    updatePlayedWords(chosenWord, score){
+        var playedWords = document.getElementById("playedWords");
+        playedWords.setAttribute('style', 'white-space: pre');
+        playedWords.innerHTML = playedWords.innerHTML + chosenWord.length + " - " + createFrenchSpanLink(chosenWord) +"  "+ score +" Points \n"
+    }
+    playBotTurn(player, filter){
+        console.log(player)
+        const allPlacements = findAllPlacements(this.grid)
+        const allCorrectPlacements = getAllCorrectPlacements(allPlacements, player.letters)
+
+        if (allCorrectPlacements.length === 0) {
+            console.log("No valid words found for current grid and letters.");
+            return false;
+        }
+
+        const longestPlacement = filterWords(allCorrectPlacements, filter)
+        const placement = longestPlacement.placement
+        const chosenWord = longestPlacement.word
+        const score = calculatePlacementScore(placement, chosenWord, valeurObj, standardMultiGrid)
+        console.log("Chosen ", placement, chosenWord, score);
+
+        this.placeWord(placement, chosenWord)
+
+        const usedLetters = [...chosenWord].filter((ch, i) => placement.word[i] === "_").join('');
+        this.removeLetters(player, usedLetters)
+        this.updateScore(player, score)
+        this.updatePlayedWords(chosenWord, score)
+        this.drawGrid()
+        return true
+    }
+    drawLetters(player){
+        if (this.letterBag.length == 0){
+            return false
+        }
+        for(let i = player.letters.length; i< 7 && this.letterBag.length > 0; i++){
+            let rnd = Math.floor(Math.random() * this.letterBag.length)
+            player.letters += this.letterBag[rnd]
+            this.letterBag = this.letterBag.slice(0, rnd) + this.letterBag.slice(rnd + 1)
+        }
+        return true
+    }
+    removeLetters(player, letters) {
+        const current = player.letters;
+        player.letters = [...letters].reduce((res, ch) => {
+            const i = res.indexOf(ch);
+            return i === -1 ? res : res.slice(0, i) + res.slice(i + 1);
+        }, current);
+    }
+
+    newTurn(){
+        this.turn++
+        let player = this.turn%2==1? this.p1 : this.p2;
+        let isStuck = false
+        if (player.type == "computer"){
+            if(!this.playBotTurn(player, Filters.MOSTPOINTS)){
+                isStuck = true
+            }
+        }
+        else{
+            //player turn
+        }
+        if ((this.drawLetters(player) || player.letters.length > 0) && !isStuck){
+            this.newTurn()
+        }
+        else{
+            console.log("Game Ended : player 1 has " + this.p1.score +
+                 " points and player 2 has " + this.p2.score + " points.")
+        }
+    
+    }
+    
+    placeWord(placement, word){
+        let placed = false
+        if (placement.direction == "horizontal"){
+            if(placement.x+word.length<16){
+                for(let i=0; i< word.length; i++){
+                    this.grid[placement.x+i][placement.y] = word[i]
+                }
+                placed = true
+            }
+        } 
+        if (placement.direction == "vertical"){
+            if(placement.y+word.length<16){
+                for(let i=0; i< word.length; i++){
+                    this.grid[placement.x][placement.y+i] = word[i]
+                }
+                placed = true
+            }
+        } 
+        return placed
+    }
 }
 
 function inputHandler(canvas, e, controller){
+    var scale = canvas.getBoundingClientRect().width / canvas.offsetWidth;
     let rect = canvas.getBoundingClientRect();
     let x = e.clientX - rect.left;
     let y = e.clientY - rect.top;
-    controller.getInput(x, y);
+    controller.getInput(x * scale, y * scale);
 }
 
 function initGrid(width, height){
@@ -46,55 +147,14 @@ function initGrid(width, height){
 
 function init(){
     var grid = initGrid(15, 15)
-    var canvas = document.getElementById("scrabbleBoard");
-    console.log(placeWord(grid, { x:4, y:8, direction:"vertical"}, "EH"))
+    var canvas = document.getElementById("scrabbleBoard");  
     var dawg = new Dawg()
     dawg.setup(FRENCH_DICTIONNARY)
 
-    var game = new Game(15, 15, 40, "player", "computer", grid, canvas);
-
-    document.getElementById("scrabbleBoard").addEventListener('click', function(e) { 
-        inputHandler(document.getElementById("scrabbleBoard"), e, game)});
-
-    function findAllValidWords(grid, letterBag) {
-        const letters = pickLetters(letterBag, 7).letters;
-        const allPossiblePlacements = findAllPlacements(grid);
-        //console.log("Total placements:", allPossiblePlacements.length);
-
-        const allCorrectPlacements = getAllCorrectPlacements(allPossiblePlacements, letters)
-        //console.log("Count of valid placements:", allCorrectPlacements.length);
-
-        if (allCorrectPlacements.length === 0) {
-            console.log("No valid words found for current grid and letters.");
-            return false;
-        }
-
-        //const [placement, validWords] = allCorrectPlacements[Math.floor(Math.random() * allCorrectPlacements.length)];
-        //const chosenWord = validWords[Math.floor(Math.random() * validWords.length)];
-
-        const longestPlacement = filterWords(allCorrectPlacements, Filters.MOSTPOINTS)
-        const placement = longestPlacement.placement
-        const chosenWord = longestPlacement.word
-        const score = calculatePlacementScore(placement, chosenWord, valeurObj, standardMultiGrid)
-        console.log("Chosen ", placement, chosenWord, score);
-
-        placeWord(grid, placement, chosenWord)
-        
-        
-        var playedWords = document.getElementById("playedWords");
-        playedWords.setAttribute('style', 'white-space: pre');
-        playedWords.innerHTML = playedWords.innerHTML + chosenWord.length + " - " + createFrenchSpanLink(chosenWord) +"  "+ score +" Points \n"
-        displayGrid(grid, standardMultiGrid, standardColor, canvas, 40, null, valeurObj, true)
-        return true
-    }
-
-    var letterBag = sacDeLettres;
-
-    const startTime2 = performance.now()
+    var game = new Game(15, 15, 40, "computer", "computer", grid, canvas, sacDeLettres);
     
-    for(let i=0; i< 30;i++){
-        findAllValidWords(grid, letterBag)
-    }
-    const endTime2 = performance.now()
-    console.log(`found all the possible words in ${(endTime2 - startTime2)/1000} seconds`) 
+    game.placeWord({ x:5, y:5, direction:"vertical"}, "SALUT")
+    game.drawLetters(game.p1)
+    game.drawLetters(game.p2)
+    game.newTurn()
 }
---
+++ TODO.md
@@ -0,0 +1,8 @@
+# Scrubble
+
+* Setup correct css
+* Input system
+* Score
+* Menu/Reset
+* Add info struct to contain all misc game info
+* Game summary
